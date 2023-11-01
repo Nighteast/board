d21ee9bbabf712f5c9091584798ba8f23927a3cb
Nighteast <jung946244@naver.com>
2023-11-01 23:33:15 +0900

    게시글 작성 처리

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -1,12 +1,17 @@
 package com.study.board.controller;
 
 import com.study.board.entity.Board;
+import com.study.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BoardController {
+
+    @Autowired
+    private BoardService boardService;
 
     @GetMapping("/board/write") //localhost:8080/board/write
     public String boardWriteForm() {
@@ -17,7 +22,7 @@ public class BoardController {
     @PostMapping("/board/writePro")
     public String boardWritePro(Board board) {
 
-        System.out.println("board.getTitle() = " + board.getTitle());
+        boardService.write(board);
         
         return "";
     }
