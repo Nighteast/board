1fe7897bbbd64beba4d128552dd94410ec354fb2
Nighteast <jung946244@naver.com>
2023-11-04 21:10:12 +0900

    게시글 수정 후 처리 메시지 출력

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -15,12 +15,14 @@ public class BoardController {
     @Autowired
     private BoardService boardService;
 
+    // 게시글 작성
     @GetMapping("/board/write") //localhost:8080/board/write
     public String boardWriteForm() {
 
         return "boardwrite";
     }
 
+    // 게시글 작성 프로세스
     @PostMapping("/board/writepro")
     public String boardWritePro(Board board, Model model) {
 
@@ -32,6 +34,7 @@ public class BoardController {
         return "message";
     }
 
+    // 게시글 리스트
     @GetMapping("/board/list")
     public String boardList(Model model) {
 
@@ -40,6 +43,7 @@ public class BoardController {
         return "boardlist";
     }
 
+    // 게시글 보기
     @GetMapping("/board/view")  // localhost:8080/board/view?id=1
     public String boardView(Model model, Integer id) {
 
@@ -48,6 +52,7 @@ public class BoardController {
         return "boardview";
     }
 
+    // 게시글 수정, 업데이트
     @GetMapping("/board/modify/{id}")
     public String boardModify(@PathVariable("id") Integer id,
                               Model model) {
@@ -58,20 +63,26 @@ public class BoardController {
         return "boardmodify";
     }
 
+    // 게시글 수정, 업데이트
     @PostMapping("/board/update/{id}")
     public String boardUpdate(@PathVariable("id") Integer id,
-                              Board board) {
+                              Board board, Model model) {
         // 기존 내용 가져오기
         Board boardTemp = boardService.boardView(id);
+
         // 기존 내용에 수정된 내용 덮어쓰기
         boardTemp.setTitle(board.getTitle());
         boardTemp.setContent(board.getContent());
-
         boardService.boardWrite(boardTemp);
 
-        return "redirect:/board/list";
+        // 게시글 수정 후 메시지 처리
+        model.addAttribute("message", "게시글이 수정 되었습니다.");
+        model.addAttribute("searchUrl", "/board/list");
+
+        return "message";
     }
 
+    // 게시글 삭제
     @GetMapping("board/delete")
     public String boardDelete(Integer id) {
 
